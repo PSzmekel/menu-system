73998fa5879364d7e2767873e54d51d4a45aff1b
Patryk Szmekel <patryk.szmekel@gmail.com>
2021-08-23 00:37:28 +0200

    add sample

--- api.py
+++ api.py
@@ -216,4 +216,4 @@ def listDish():
 mailing(1).start()
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=os.environ['PORT'], debug=True)
--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
